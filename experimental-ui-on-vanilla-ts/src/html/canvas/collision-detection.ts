2dce83348250ab1e7870c8652222ec69b51fc088
Artsem Nikitsenka <artsem_nikitsenka@epam.com>
2026-07-28 09:18:49 +0300

    Add fire circles game

--- experimental-ui-on-vanilla-ts/src/html/canvas/collision-detection.ts
+++ experimental-ui-on-vanilla-ts/src/html/canvas/collision-detection.ts
@@ -1,38 +1,4 @@
-import { euclideanDistance } from "@/math/geometry"
-
-interface CircleParams {
-  x: number
-  y: number
-  radius: number
-  color: string
-}
-
-class Circle {
-  x: number
-  y: number
-  radius: number
-  color: string
-
-  constructor({ x, y, radius, color }: CircleParams) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
-  }
-
-  draw(ctx: CanvasRenderingContext2D) {
-    ctx.beginPath()
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    ctx.fillStyle = this.color
-    ctx.fill()
-    ctx.stroke()
-    ctx.closePath()
-  }
-
-  update(ctx: CanvasRenderingContext2D) {
-    this.draw(ctx)
-  }
-}
+import { euclideanDistance, Circle } from "@/math/geometry"
 
 function createCircles() {
   return [
