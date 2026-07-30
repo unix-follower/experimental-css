3e783b3e68845d19c92534a2d6d17fec81ce4cf1
Artsem Nikitsenka <artsem_nikitsenka@epam.com>
2026-07-30 20:52:43 +0300

    Fix CI

--- experimental-ui-on-astro/astro.config.mjs
+++ experimental-ui-on-astro/astro.config.mjs
@@ -5,6 +5,8 @@ import react from "@astrojs/react"
 
 import tailwindcss from "@tailwindcss/vite"
 
+import node from "@astrojs/node"
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
@@ -12,4 +14,8 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: node({
+    mode: "standalone",
+  }),
 })
