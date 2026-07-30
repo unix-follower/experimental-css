93150cdc25157d0b4d102d0358c2c0911cea6772
unix-follower <61003331+unix-follower@users.noreply.github.com>
2026-07-30 20:56:00 +0300

    Add a blog example built with Astro (#40)
    Co-authored-by: Artsem Nikitsenka <artsem_nikitsenka@epam.com>

--- experimental-ui-on-astro/astro.config.mjs
+++ experimental-ui-on-astro/astro.config.mjs
@@ -3,7 +3,19 @@ import { defineConfig } from "astro/config"
 
 import react from "@astrojs/react"
 
+import tailwindcss from "@tailwindcss/vite"
+
+import node from "@astrojs/node"
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  adapter: node({
+    mode: "standalone",
+  }),
 })
